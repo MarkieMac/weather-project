a4e813f1a51b540404c698b54cc6e9824b78a458
MarkieMac <101209598+MarkieMac@users.noreply.github.com>
2022-06-16 16:52:46 -0500

    updated api and css

--- src/index.js
+++ src/index.js
@@ -26,25 +26,35 @@ let day =days[date.getDay()];
 return `${day} ${hours}:${minutes}`;
 } 
 
+function formatDay(timestamp) {
+let date = new Date (timestamp * 1000);
+let day = date.getDay();
+let days= ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+ return days[day];
+}
 function displayForecast(response) {
+  let forecast = response.data.daily; 
+
   let forecastElement = document.querySelector("#forecast");
-  let days = ["Thu", "Fri", "Sat", "Sun"];
+  
   let forecastHTML = `<div class="row">`;
   
-  days.forEach(function (day) {
+  forecast.forEach(function (forecastDay, index) {
+   
  
   forecastHTML = forecastHTML + 
   `
     <div class="col-3">
       <div class="weather-forecast-date">
-     ${day}    </div> 
+     ${formatDay(forecastDay.dt)}    </div> 
       <img 
-      src="http://openweathermap.org/img/wn/50d@2x.png"
+      src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
       alt=""
       width="42"/>
       <div class-="weather-forecast-temperatures">
-      <span class="weather-forecast-max">89°</span>
-      <span  class="weather-forecast-min">73°</span>
+      <span class="weather-forecast-max">${Math.round(forecastDay.temp.max)}°</span>
+      <span  class="weather-forecast-min">${Math.round(forecastDay.temp.min)}°</span>
      
    
     </div>
@@ -52,7 +62,9 @@ function displayForecast(response) {
 
 
 `;
+    
 });
+
 forecastHTML = forecastHTML + `</div>`;
 forecastElement.innerHTML = forecastHTML;
 }
